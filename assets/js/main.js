c0ccacd717f2b88cb4a2cd9e746bde4c98e0d39b
Brandon Long <blong1996@gmail.com>
2016-06-25 15:43:26 -0400

    Add back to top button

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,15 @@
 
 
 (function($) {
-	
+	var amountScrolled = 300;
+
+	$(window).scroll(function() {
+		if ( $(window).scrollTop() > amountScrolled ) {
+			$('a.back-to-top').fadeIn('slow');
+		} else {
+			$('a.back-to-top').fadeOut('slow');
+		}
+	});
 
 	skel.breakpoints({
 		xlarge:  '(max-width: 1680px)',
